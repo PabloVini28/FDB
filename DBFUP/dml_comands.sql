f2a7eb074e604d340fb837ffaf2ca8b78fad8d77
Pablo Vinícios <128994831+PabloVini28@users.noreply.github.com>
2024-11-10 21:48:41 -0300

    Delete DBFUP directory

--- DBFUP/dml_comands.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO "nome da tabela" (nome, idade) VALUES('JOAO',39);
-UPDATE "nome da tabela" SET idade = 31 WHERE nome = 'Joao';
-DELETE FROM "nome da tabela" WHERE nome = 'Joao';
-SELECT * FROM "nome da tabela"; 
