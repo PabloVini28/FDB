0838643dc90dc529af135f894e20b05c002ecabf
Pablo Vinícios <128994831+PabloVini28@users.noreply.github.com>
2025-02-21 15:15:46 -0300

    Update Q1.sql

--- AP2/Q1.sql
+++ AP2/Q1.sql
@@ -1,5 +1,5 @@
 select V.numero_voo, Aero.nome, Aero.cidade, Aero.pais
 from Voo V 
 join Aeroporto Aero on V.id_aeroporto_origem = Aero.id_aeroporto
-where Aero.pais = 'Brasil'
+where Aero.pais like 'Brasil'
 order by Aero.nome asc;
